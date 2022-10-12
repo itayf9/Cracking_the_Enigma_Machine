a7a9b694b80628dc3bc2d02f83d97e92f7370ae0
Itay Finkelshtein <itayf9@gmail.com>
2022-10-12 23:32:33 +0300

    Add allie setting

---
+++ AlliesClient/AlliesClient/src/body/screen1/alliesetting/AllieSettingController.java
@@ -0,0 +1,44 @@
+package body.screen1.alliesetting;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.input.MouseEvent;
+
+public class AllieSettingController {
+
+    @FXML
+    private Spinner<Integer> taskSizeSpinner;
+
+    @FXML
+    private Button isReadyButton;
+
+
+    @FXML
+    private void initialize() {
+//        taskSizeSpinner.valueFactoryProperty().set(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, (totalPossibleWindowsPositions.getValue().intValue())));
+//
+//
+//        taskSizeSpinner.getEditor().textProperty().addListener((observable, oldValue, newValue) -> {
+//            try {
+//                int newValueAsInt = Integer.parseInt(newValue);
+//                if (newValueAsInt > 0 && newValueAsInt <= totalPossibleWindowsPositions.getValue()) {
+//                    taskSizeSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, totalPossibleWindowsPositions.getValue().intValue(), newValueAsInt));
+//                } else {
+//                    taskSizeSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, totalPossibleWindowsPositions.getValue().intValue(), Integer.parseInt(oldValue)));
+//                    taskSizeSpinner.getEditor().setText(oldValue);
+//                }
+//            } catch (NumberFormatException e) {
+//                taskSizeSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, totalPossibleWindowsPositions.getValue().intValue(), Integer.parseInt(oldValue)));
+//                taskSizeSpinner.getEditor().setText(oldValue);
+//            }
+//        });
+    }
+
+    @FXML
+    void setReady(MouseEvent event) {
+
+    }
+
+}
