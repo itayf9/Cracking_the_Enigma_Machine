0201156c5883d4079e1254ef045f64f902f56950
Itay Finkelshtein <itayf9@gmail.com>
2022-10-24 23:02:26 +0300

    Move dependencies of external jars to EnigmaUtility

--- EnigmaUtillity/src/http/url/URLconst.java
+++ EnigmaUtillity/src/http/url/URLconst.java
@@ -2,7 +2,7 @@ package http.url;
 
 
 public class URLconst {
-    public static final String BASE_URL = "http://localhost:8080/BattleFieldServer_Web_exploded";
+    public static final String BASE_URL = "http://localhost:8080/EnigmaWebServer";
 
     public static final String LOGIN_SRC = "/login";
 
